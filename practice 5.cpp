45b61698726e8948c086b4800a53799fc31728a4
Mounota Das <168008578+MounotaDas@users.noreply.github.com>
2024-05-17 20:36:33 +0600

    Add files via upload

---
+++ practice 5.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace  std;
+
+float multiplier(float a,float b){
+return a*b;
+
+}
+int main(){
+
+float a,b;
+
+cin>>a>>b;
+cout<<multiplier(a,b)<<endl;
+
+return 0;
+
+}
