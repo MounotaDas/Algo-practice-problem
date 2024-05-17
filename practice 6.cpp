4b7432469e6784e6ac86e55c5fe479d1eef457a2
Mounota Das <168008578+MounotaDas@users.noreply.github.com>
2024-05-17 20:38:51 +0600

    Add files via upload

---
+++ practice 6.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+void uppercase(string s){
+    for(int i=0;i<s.size();i++)
+    {
+        char l=s[i]-32;cout<<l;
+    }}
+int main(){
+    string s; cin>>s;uppercase(s);
+    }
+
