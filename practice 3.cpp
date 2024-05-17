e014fc60c19e2720c1d8a0454134deee0c6dd994
Mounota Das <168008578+MounotaDas@users.noreply.github.com>
2024-05-17 20:35:26 +0600

    Add files via upload

---
+++ practice 3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double meter_to_feet(double n)
+{
+    return n*3.28084;
+}
+
+int main(){
+double n;
+cout<<meter_to_feet(n)<<endl;
+cin>>n;
+
+return 0;
+
+}
